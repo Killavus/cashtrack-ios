23448258201aa913caea49b386cbe0bb9ac4786d
Marcin Grzywaczewski <killavus@gmail.com>
2014-11-07 22:18:30 +0100

    Initial Commit

---
+++ CashTrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CashTrack
+//
+//  Created by Marcin Grzywaczewski on 07/11/14.
+//  Copyright (c) 2014 Marcin Grzywaczewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
